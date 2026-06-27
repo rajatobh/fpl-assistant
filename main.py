213c8861539df67de7e1a03edc5df32c6670f290
Rajat Obheroi <ronra@MacBook-Air-2.local>
2026-06-26 23:10:37 -0700

    feat: add position column to table

--- main.py
+++ main.py
@@ -8,12 +8,17 @@ data = response.json()
 
 players = data['elements']
 
-print("-" * 60)
-print(f"| {'Name':<30} | {'Price':<10} | {'Points':<10} |")
-print("-" * 60)
+element_types = data['element_types']
+
+positions = {t['id']: t['singular_name_short'] for t in element_types}
+
+print("-" * 70)
+print(f"| {'Name':<30} | {'Pos':<5} | {'Price':<10} | {'Points':<10} |")
+print("-" * 70)
 
 for player in players[:10]:
     name = f"{player['first_name']} {player['second_name']}"
     price = f"£{player['now_cost']/10}m"
     points = player['total_points']
-    print(f"| {name:<30} | {price:<10} | {points:<10} |")
+    position = positions[player['element_type']]
+    print(f"| {name:<30} | {position:<5} | {price:<10} | {points:<10} |")
