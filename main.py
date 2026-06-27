ade3559bdc8978f2ff54ed04904faa5fa6b3fd47
Rajat Obheroi <ronra@MacBook-Air-2.local>
2026-06-26 23:23:45 -0700

    feat: added player team name to table

--- main.py
+++ main.py
@@ -12,13 +12,16 @@ element_types = data['element_types']
 
 positions = {t['id']: t['singular_name_short'] for t in element_types}
 
-print("-" * 70)
-print(f"| {'Name':<30} | {'Pos':<5} | {'Price':<10} | {'Points':<10} |")
-print("-" * 70)
+teams = {t['id']: t['name'] for t in data['teams']}
+
+print("-" * 97)
+print(f"| {'Name':<30} | {'Team':<25} | {'Pos':<5} | {'Price':<10} | {'Points':<10} |")
+print("-" * 97)
 
 for player in players[:10]:
     name = f"{player['first_name']} {player['second_name']}"
     price = f"£{player['now_cost']/10}m"
     points = player['total_points']
     position = positions[player['element_type']]
-    print(f"| {name:<30} | {position:<5} | {price:<10} | {points:<10} |")
+    team = teams[player['team']]
+    print(f"| {name:<30} | {team:<25} | {position:<5} | {price:<10} | {points:<10} |")
